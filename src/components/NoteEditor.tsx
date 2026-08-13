1c779e9691fef0afbb1f040b6a6b96695fc6002a
kray55 <108510997+kray55@users.noreply.github.com>
2026-08-13 12:35:20 +1000

    Fix import statement for TextStyle extension

--- src/components/NoteEditor.tsx
+++ src/components/NoteEditor.tsx
@@ -6,7 +6,7 @@ import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
 import Link from "@tiptap/extension-link";
 import Placeholder from "@tiptap/extension-placeholder";
-import TextStyle from "@tiptap/extension-text-style";
+import { TextStyle } from "@tiptap/extension-text-style";
 import Color from "@tiptap/extension-color";
 import Highlight from "@tiptap/extension-highlight";
 import TaskList from "@tiptap/extension-task-list";
